69d860eb21b6daa2610f99cc80c4379048125e56
Woochang1132 <show4031132@gmail.com>
2023-10-25 10:46:42 +0900

    최우창 react-router 서비스 활용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+//import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Videos from "./pages/Videos";
+import Root from "./pages/Root";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+  },
+
+  {
+    path: "/videos",
+    element: <Videos />,
+  },
+]);
 
 function App() {
-  return 
-  (
-
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
